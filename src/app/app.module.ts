03a161146943e2c00212b2eee8d993e2dea1876f
rashminmhtc <rashminmhtc@gmail.com>
2018-05-30 23:48:31 +0530

    Routing changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,23 @@ import { RouterModule, Routes }   from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { PeopleComponent } from './component/people/people.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'people', component: PeopleComponent },
+  { path: 'dashboard', component: DashboardComponent }
+]; // path to specify URL, component to be mapped the route to, redirectTo
 
 @NgModule({
   declarations: [
     AppComponent,
-    PeopleComponent
+    PeopleComponent,
+    DashboardComponent
   ], // components in the project
   imports: [
     BrowserModule,
-    RouterModule.forRoot([
-      { path: '', redirectTo: 'component/people', pathMatch: 'full' },
-      { path: 'people', component: PeopleComponent }
-    ])
+    RouterModule.forRoot( routes )  // routerModule provides - "router-outlet" (used in template to specify where to display) & "router-link" navigation between routes 
   ], // modules used
   providers: [], // services
   bootstrap: [AppComponent] // main component
